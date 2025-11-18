e98855208ab96b007a088eb150c89e8d07819b96
Ryker Fish <rfish@mines.edu>
2025-11-18 13:43:07 -0700

    add support for flat shapes and lists

--- src/Rigid.py
+++ src/Rigid.py
@@ -3,6 +3,12 @@ import numpy as np
 from typing import TypeAlias
 
 vector: TypeAlias = list | np.ndarray
+"""Rigid body interface for Python.
+
+Dev notes:
+- The C++ code expects inputs as flattened numpy arrays. There's a decent bit of wrangling done in this Python interface to make it work seamlessly with lists & multi-dimensional arrays.
+"""
+
 
 class RigidBody:
     X_shape: tuple[int, ...]
@@ -45,8 +51,8 @@ class RigidBody:
 
     def set_config(self, X: vector, Q: vector) -> None:
         self.__check_and_set_configs(X, Q)
-        X = np.array(X).flatten()
-        Q = np.array(Q).flatten()
+        X = np.array(X).ravel()
+        Q = np.array(Q).ravel()
         self.cb.setConfig(X, Q)
         self.cb.set_K_mats()
 
@@ -56,27 +62,27 @@ class RigidBody:
         shape = (-1, 3) if len(self.X_shape) == 2 else (-1)
         return np.array(self.cb.multi_body_pos()).reshape(shape)
 
-    def KT_dot(self, lambda_vec: vector) -> np.ndarray:
-        self.__check_input_size(lambda_vec=lambda_vec)
-        result = self.cb.KT_x_Lam(lambda_vec)
-        shape = (-1, 3) if len(self.X_shape) == 2 else (-1)
-        return np.array(result).reshape(shape)
-
     def K_dot(self, U: vector) -> np.ndarray:
         self.__check_input_size(U_vec=U)
-        result = self.cb.K_x_U(U)
-        shape = (-1, 3) if len(self.X_shape) == 2 else (-1)
-        return np.array(result).reshape(shape)
+        result = self.cb.K_x_U(np.array(U).ravel())
+        shape = (-1, 3) if np.ndim(U) == 2 else (-1)
+        return result.reshape(shape)
+
+    def KT_dot(self, lambda_vec: vector) -> np.ndarray:
+        self.__check_input_size(lambda_vec=lambda_vec)
+        result = self.cb.KT_x_Lam(np.array(lambda_vec).ravel())
+        shape = (-1, 3) if np.ndim(lambda_vec) == 2 else (-1)
+        return result.reshape(shape)
 
     def apply_PC(self, lambda_vec: vector, U_vec: vector) -> np.ndarray:
         self.__check_input_size(lambda_vec=lambda_vec, U_vec=U_vec)
-        in_vec = np.concatenate((lambda_vec, U_vec))
+        in_vec = np.concatenate((np.array(lambda_vec).ravel(), np.array(U_vec).ravel()))
         return self.cb.apply_PC(in_vec)
 
     def apply_M(self, F: vector) -> np.ndarray:
         self.__check_input_size(lambda_vec=F)
         r_vecs = self.get_blob_positions()
-        return self.cb.apply_M(F, r_vecs.flatten())
+        return self.cb.apply_M(np.array(F).ravel(), r_vecs.ravel())
 
     def get_K(self) -> np.ndarray:
         return self.cb.get_K()
@@ -84,11 +90,11 @@ class RigidBody:
     def get_Kinv(self) -> np.ndarray:
         return self.cb.get_Kinv()
 
-    def evolve_rigid_bodies(self, U: vector):
+    def evolve_rigid_bodies(self, U: vector) -> None:
         self.__check_input_size(U_vec=U)
-        self.cb.evolve_X_Q(U)
+        self.cb.evolve_X_Q(np.array(U).ravel())
 
-    def __check_and_set_configs(self, X: vector, Q: vector):
+    def __check_and_set_configs(self, X: vector, Q: vector) -> None:
         x_size = np.prod(np.shape(X))
         q_size = np.prod(np.shape(Q))
 
